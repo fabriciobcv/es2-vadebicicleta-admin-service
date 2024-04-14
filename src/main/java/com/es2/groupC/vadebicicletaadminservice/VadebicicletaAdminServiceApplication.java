b17bb76ff664a7d44c863a25da5f057b52d0659c
fabriciobcv <fabriciobcvpessoal@gmail.com>
2024-04-14 17:38:24 -0300

    commit inicial

---
+++ src/main/java/com/es2/groupC/vadebicicletaadminservice/VadebicicletaAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.es2.groupC.vadebicicletaadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VadebicicletaAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VadebicicletaAdminServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es2/groupC/vadebicicletaadminservice/VadebicicletaAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.groupC.vadebicicletaadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VadebicicletaAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
